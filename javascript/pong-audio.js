4daec75b34fd1fd7a2393e79f121770d3bef5c84
KingCharno <133684466+KingCharno@users.noreply.github.com>
2023-05-30 09:54:54 -0500

    Update pong-audio.js
    Changed paddle sound

--- javascript/pong-audio.js
+++ javascript/pong-audio.js
@@ -46,7 +46,7 @@ export var soundArray = []; //list of sounds loaded
 export var wallSound = new soundFile("Sonar-ping-sound.mp3"); //load sound
 soundArray.push(wallSound); //add sound to list of sounds
 
-export var paddleSound = new soundFile("paddle.mp3");
+export var paddleSound = new soundFile("Deep Ocean Monster Noise.mp3");
 soundArray.push(paddleSound);
 
 export var scoreSound = new soundFile("silence.mp3");
